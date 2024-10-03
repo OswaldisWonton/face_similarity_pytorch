0aedf942ff2a5fcc2a8032e5eac8a39a2258f507
Bin Wang <106138409+WangbinIntel@users.noreply.github.com>
2024-10-03 16:58:04 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y wget
-wget https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.tar.gz
-tar -zxvf cmake-3.22.2-linux-x86_64.tar.gz
-sudo cp -r cmake-3.22.2-linux-x86_64/* /usr/local/
-cmake --version
+
